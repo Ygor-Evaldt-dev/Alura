ca78291260acbc9137094b985f6d4154d4a3801c
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-04-14 21:39:27 -0300

    refatorado jogo adivinhacao / adicionado README.md

---
+++ linguagem-c/jogo-adivinhação/README.md
@@ -0,0 +1,5 @@
+Abra o arquivo "adivinhacao.c" no VSCode e faça a compilação.
+
+O jogo tem 3 níveis, quando maior é o nível menor as chances de acertar o número secreto.
+
+BOA SORTE !!
--- linguagem-c/jogo-adivinhação/adivinhaçao.c
+++ linguagem-c/jogo-adivinhação/adivinhaçao.c
@@ -1,57 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <windows.h>
 #include <time.h>
+#include <windows.h>
 
 int main() {
   UINT CPAGE_UTF8 = 65001;
   SetConsoleOutputCP(CPAGE_UTF8);
 
-  
-  int tentativa = 0, chute, numeroSecreto;
-  float pontos = 1000.0;
-
-  printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-  printf("BEM VINDO AO JOGO DA ADIVINHAÇÃO\n");
-  printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-  printf("O computador pensou em um número de 0 a 20, tente adivinhar qual número foi. \n");
-
-  /*Número randomico*/
+  float pontos = 1000, pontosPerdidos = 0;
   int segundos = time(0);
   srand(segundos);
-  numeroSecreto = rand() % 20;
 
-  while( chute != numeroSecreto){
-    printf("Tentativa %d. Que número você escolhe? ", tentativa + 1);
-    scanf("%d%*c", &chute);
-    tentativa++;
-    if (chute == numeroSecreto && tentativa > 10) {
-      pontos = pontos - ((chute - numeroSecreto) / (float)2);
-      printf("Você acertou na tentativa %d, hoje não é seu dia de sorte.\n", tentativa);
-      printf("Total de pontos %.1f", pontos);
-      break;
+  int numeroSecreto = rand() %50, numTentativas, opcao, chute;
+  printf("       _______                                                \n");
+  printf("     _/       \\_                                             \n");
+  printf("    / |       | \\                                            \n");
+  printf("   /  |__   __|  \\                                           \n");
+  printf("  |__/((o| |o))\\__|                                          \n");  
+  printf("  |      | |      |                                           \n");
+  printf("  |\\     |_|     /|                                          \n");
+  printf("  |\\           /  |        Olá humano!!                      \n");
+  printf("   \\| /  ___  \\ |/     --  Bem vindo ao jogo da adivinhação.\n");
+  printf("    \\ | / _ \\ | /                                           \n");
+  printf("     \\_________/                                             \n");
+  printf("      _|_____|_                                               \n");
+  printf(" ____|_________|____                                          \n");
+  printf("/                   \\                                        \n");
+  printf("\n\n");
+  printf("Pensei em um número inteiro secreto entre 0 e 50, tente adivinhar qual é esse número\n\n");
+  printf("Em qual nível você quer jogar:\n");
+  printf("(1) - Fácil (2) - Médio (3) - Difícil\n");
+  printf("Opção: ");
+  scanf("%d%*c", &opcao);
+  
+  switch (opcao) {
+  case 1:
+    printf("Você tem 15 tentativas para acertar o número\n");
+    numTentativas = 15;
+    for (numTentativas ; numTentativas >= 0; numTentativas--) {
+      printf("Tentativa %d. Qual seu chute? ", numTentativas);
+      scanf("%d%*c", &chute);
+      if (chute == numeroSecreto) {
+        printf("É seu dia de sorte humano\nPARABÉNS\nVocê acertou meu número secreto.\n");
+        printf("Pontuação: %.1f", pontos);
+        break;
+      }
+      else {
+        printf("Você errou!\n");
+        if (chute > numeroSecreto) {
+          printf("Seu chute é maior que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+        else if (chute < numeroSecreto) {
+          printf("Seu chute é menor que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+      }
+      if (numTentativas == 0) {
+        printf("Tentativas esgotadas\nGAME OVER!!\n");
+      } 
     }
-    else if (chute == numeroSecreto) {
-      pontos = pontos - ((chute - numeroSecreto) / (float)2);
-      printf("Parabens!! Você acertou na tentativa %d.\n", tentativa);
-      printf("Total de pontos %.1f", pontos);
-      break;
+    break;
+  case 2:
+    printf("Você tem 10 tentativas para acertar o número\n");
+    numTentativas = 10;
+    for (numTentativas ; numTentativas >= 0; numTentativas--) {
+      printf("Tentativa %d. Qual seu chute? ", numTentativas);
+      scanf("%d%*c", &chute);
+      if (chute == numeroSecreto) {
+        printf("É seu dia de sorte humano\nPARABÉNS\nVocê acertou meu número secreto.\n");
+        printf("Pontuação: %.1f", pontos);
+        break;
+      }
+      else {
+        printf("Você errou!\n");
+        if (chute > numeroSecreto) {
+          printf("Seu chute é maior que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+        else if (chute < numeroSecreto) {
+          printf("Seu chute é menor que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+      }
+      if (numTentativas == 0) {
+        printf("Tentativas esgotadas\nGAME OVER!!\n");
+      } 
     }
-    else if (chute < 0) {
-      printf("Você não pode escolher números negativos\n");
-      tentativa--;
-    }   
-    else if (chute < numeroSecreto) {
-      pontos = pontos - (abs(chute - numeroSecreto) / (float)2);
-      printf("Você errou.\n");
-      printf("O número escolhido foi menor que o número secreto\n");
+    break;
+  case 3:
+    printf("Você tem 6 tentativas para acertar o número.\n");
+    numTentativas = 6;
+    for (numTentativas ; numTentativas > 0; numTentativas--) {
+      printf("Tentativa %d. Qual seu chute? ", numTentativas);
+      scanf("%d%*c", &chute);
+      if (chute == numeroSecreto) {
+        printf("É seu dia de sorte humano\nPARABÉNS\nVocê acertou meu número secreto.\n");
+        printf("Pontuação: %.1f", pontos);
+        break;
+      }
+      else {
+        printf("Você errou!\n");
+        if (chute > numeroSecreto) {
+          printf("Seu chute é maior que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+        else if (chute < numeroSecreto) {
+          printf("Seu chute é menor que meu número secreto.\n");
+          pontosPerdidos = (abs(numeroSecreto - chute) / (float)2);
+          pontos -= pontosPerdidos;
+        }
+      }
+      if (numTentativas == 1) {
+        printf("Tentativas esgotadas\nGAME OVER!!\n");
+      } 
     }
-    else if (chute > numeroSecreto) {
-      pontos = pontos - ((chute - numeroSecreto) / (float)2);
-      printf("Você errou.\n");
-      printf("O número escolhido foi maior que o número secreto\n");
-    }
+    break;
   }
   return 0;
 }
